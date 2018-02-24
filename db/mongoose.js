488184939f23eb54e7ade7d134f3c3cfbba8a037
Remote machine <remote@example.com>
2018-02-25 01:14:51 +0300

    Add mongodb zero version

---
+++ db/mongoose.js
@@ -0,0 +1,46 @@
+var mongoose    = require('mongoose');
+
+var db = mongoose.createConnection('mongodb://localhost');
+
+db.on('error', function (err) {
+    console.log('connection error:', err.message);
+});
+db.once('open', function callback () {
+    console.log("Connected to DB!");
+});
+
+var Schema = mongoose.Schema;
+
+// Schemas
+var Team = new Schema({
+    name: { type: String, required: true },
+    image_url: { type: String, required: true }
+});
+
+var Session = new Schema({
+    name: { type: String, required: true },
+    teams: [Team]
+});
+
+var Voter = new Schema({
+    session: Session,
+    voter_id: { type: String, required: true }
+});
+
+var StatVotes = new Schema({
+    session: Session,
+    round: { type: String, required: true },
+    voter_id: { type: String, required: true },
+    team: Team
+});
+
+var SessionModel = mongoose.model('Session', Session);
+var VoterModel = mongoose.model('Voter', Voter);
+var StatModel = mongoose.model('StatVotes', StatVotes);
+var TeamModel = mongoose.model('Team', Team);
+
+module.exports.SessionModel = SessionModel;
+module.exports.VoterModel = VoterModel;
+module.exports.StatModel = StatModel;
+module.exports.TeamModel = TeamModel;
+
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+var TeamModel = require('../db/mongoose').TeamModel;
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* POST vote */
+router.post('/vote', function(req, res, next) {
+  console.log(req.body)
+});
+
+router.post('/team', function(req, res) {
+    var team = new TeamModel({
+        name: req.body.name,
+        image_url: req.body.image_url
+    });
+    team.save();
+    return res.send({ status: 'OK', team: team });
 });
 
 module.exports = router;
